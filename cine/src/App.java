d6c6a046db40fed98f3cb8d4cb2720bfad99605d
edithbutp <edithbetan1@hotmail.com>
2021-07-12 09:01:34 -0500

    sobrecarga de metodos cine

---
+++ cine/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main (String[] args) throws Exception {
+        System.out.println("Bienvenido al Cine");
+        
+        Cliente clienteUno = new Cliente();
+        clienteUno.setCedula("32862030");
+        clienteUno.setNombre("EDITH");
+        clienteUno.setId(1);
+        clienteUno.generarSaludo();
+        clienteUno.generarSaludo("Buenos dias");
+        float costo = clienteUno.calcularCosto(8000);
+        clienteUno.mostrarDatos();
+
+        System.out.println();
+        System.out.println("Cedula: " + clienteUno.getCedula() + "\nNombre: "+clienteUno.getNombre()+ "\nID: " + clienteUno.getId()+ "\nCosto tiquete: "+ costo);
+
+        ClientePremium clienteDos = new ClientePremium();
+        clienteDos.setCedula("1919778");
+        clienteDos.setNombre("MARCOS");
+        clienteDos.setNumeroTarjeta(100);
+        float costoDos = clienteDos.calcularCosto(8000);
+
+        System.out.println("Cedula: " + clienteDos.getCedula() + "\nNombre: "+clienteDos.getNombre()+ "\nNumero de Tarjeta: " + clienteDos.getNumeroTarjeta()+ "\nCosto tiquete: "+ costoDos);
+
+
+    }
+}
---
+++ cine/src/ClientePremium.java
@@ -0,0 +1,26 @@
+public class ClientePremium extends Persona implements Tiquete{
+    
+    private int numeroTarjeta;
+    
+    @Override
+    public float calcularCosto(float costoBase){
+        float costoReal = (float)(costoBase - (costoBase*0.5));
+        return costoReal;
+    }
+
+    public void setNumeroTarjeta(int numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    public int getNumeroTarjeta() {
+        return this.numeroTarjeta;
+    }
+    
+        @Override
+    public void mostrarDatos() {
+        System.out.println("Cedula = " + this.cedula
+                        + "Nombre = " + this.nombre
+                        + "numero tajeta = " + this.numeroTarjeta);
+    }
+    
+}
